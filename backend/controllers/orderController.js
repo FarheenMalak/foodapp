4187cbb45ec4d0e561a1d2923527975ee16764ba
Farheen Malak <115576023+FarheenMalak@users.noreply.github.com>
2025-12-05 02:32:04 +0500

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placing user order
 const placeOrder = async (req, res) => {
-  const frontend_url = 'https://foodapp-frontend-phi.vercel.app';
+  const frontend_url = process.env.FRONT_END_URL;
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
